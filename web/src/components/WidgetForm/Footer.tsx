983018e52958b4239954a8dfb236b31dc0561661
DevMaicon <devmaicon@gmail.com>
2022-07-07 12:37:05 -0300

    url git

--- web/src/components/WidgetForm/Footer.tsx
+++ web/src/components/WidgetForm/Footer.tsx
@@ -10,7 +10,7 @@ export function Footer() {
             className={`flex items-center justify-center w-full h-12 text-xs no-underline bg-gray-200  dark:bg-dark-surface-secondary border-b-4 border-theme-${theme} rounded-b-2xl`}
         >
             <a
-                href="https://github.com/devmaicon85/widget.v2/"
+                href="https://github.com/devmaicon85/widget/"
                 className="flex no-underline text-light-text-secondary"
                 target="_blank"
                 rel="noreferrer"
