37989b74ff1b8b9cab251d95104a842eecd0ac03
DevMaicon <devmaicon@gmail.com>
2022-07-07 12:08:28 -0300

    update step 3 success

--- web/src/components/WidgetForm/Steps/Step3_Success.tsx
+++ web/src/components/WidgetForm/Steps/Step3_Success.tsx
@@ -2,10 +2,9 @@ import { CheckSquare } from "phosphor-react";
 import imageInstagramUrl from "../../../assets/instagram.png";
 import {
     useGetQueryInstagram,
-    useGetQueryTheme,
+    useGetQueryTheme
 } from "../../../util/getQueryUrl";
 import { Button } from "../../Button";
-import { Header } from "../Header";
 
 type Props = {
     onFeedbackRestartType: () => void;
@@ -17,10 +16,10 @@ export function Step3_Success({ onFeedbackRestartType }: Props) {
 
     return (
         <>
-            <Header>Finalizado!</Header>
+            {/* <Header>Finalizado!</Header> */}
             <div className="flex flex-col items-center w-full p-4 py-10">
-                <h1 className="mt-2 mb-5 text-xl">
-                    Agradecemos o seu contato!
+                <h1 className="mt-2 mb-5 text-xl text-center">
+                    Enviado com sucesso<br/>Agradecemos o seu contato
                 </h1>
                 {queryInstagram ? (
                     <a href={queryInstagram} target="_blank" rel="noreferrer">
